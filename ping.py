07221873c3fd053953961686ce135a1869457b0c
ENKO <k@narukoshin.me>
2024-11-10 21:31:21 +0200

    Updated code

--- ping.py
+++ ping.py
@@ -5,8 +5,15 @@ import sys
 url = sys.argv[1]
 
 if __name__ == '__main__':
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive'
+    }
+
     try:
-        r = requests.get(url, timeout=3)
+        r = requests.get(url, timeout=3, headers=headers)
         print(r.status_code)
     except Exception as e:
         print(e)
